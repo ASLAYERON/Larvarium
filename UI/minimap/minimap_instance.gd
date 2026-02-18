00b4c218e9eb5adc099550b7b8ae086b4ad479f7
Minenatan <natanandrelajoie@gmail.com>
2026-02-18 16:30:12 +0100

    Revert "Mergin inventory and minimap"
    This reverts commit a2c9c4be532586dd38e5be437f45dec8ea79b18b, reversing
    changes made to 632304a286eacbcd41d5d0125bed297742a48b1d.

--- UI/minimap/minimap_instance.gd
+++ UI/minimap/minimap_instance.gd
@@ -1,35 +1,12 @@
 extends Control
 
-var master : Node
-
-
-const IS_INVENTORY :bool = false
-
 @onready var texture_rect = $TextureRect
-@onready var modulate_color = texture_rect.modulate
 
 
-var vector : Vector2
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
 
 
 func initialize(level:String):
 	texture_rect.initialize(load("res://niveaux/test/"+level+"/image.gif"))
-
-func add_potion(potion):
-	modulate = modulate.lerp(Global.item_data[potion]["color"],0.5)
-
-func start_hover():
-	texture_rect.modulate = modulate.darkened(0.25)
-
-func stop_hover():
-	texture_rect.modulate = modulate_color
-
-func _on_button_pressed() -> void:
-	master.slot_pressed(self)
-
-
-func _on_button_mouse_entered() -> void:
-	master.slot_hovered(self)
-
-func _on_button_mouse_exited() -> void:
-	master.slot_not_hovered(self)

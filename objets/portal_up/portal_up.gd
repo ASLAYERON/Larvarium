87f560d2bfbb8cb683069cd7fd0c63d5808611c9
ASLAYERON <albert.boivin54@gmail.com>
2026-02-19 15:04:46 +0100

    reskin

--- objets/portal_up/portal_up.gd
+++ objets/portal_up/portal_up.gd
@@ -5,5 +5,5 @@ func _on_body_entered(body: Node2D) -> void:
 		var coord = get_parent().coord
 		var coord_x = coord[0]
 		var coord_y = coord[1]-1
-		Global.tp_offset = Vector2(body.position.x,900)
+		Global.tp_offset = Vector2(body.position.x,455)
 		get_tree().change_scene_to_file(Global.levels_map[Global.active_level][coord_y][coord_x]+"room.tscn")
